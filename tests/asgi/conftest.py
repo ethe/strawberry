3d0834a6360eed307e56fc0ae039489d296eec01
Patrick Arminio <patrick.arminio@gmail.com>
2019-06-04 22:37:11 +0100

    WIP

---
+++ tests/asgi/conftest.py
@@ -0,0 +1,41 @@
+import asyncio
+import typing
+
+import pytest
+
+import strawberry
+from starlette.testclient import TestClient
+from strawberry.asgi import GraphQL
+
+
+@pytest.fixture
+def schema():
+    @strawberry.type
+    class Query:
+        @strawberry.field
+        def hello(self, info, name: typing.Optional[str] = None) -> str:
+            return f"Hello {name or 'world'}"
+
+    @strawberry.type
+    class Subscription:
+        @strawberry.subscription
+        async def example(self, info) -> typing.AsyncGenerator[str, None]:
+            await asyncio.sleep(1.5)
+
+            yield "Hi"
+
+    return strawberry.Schema(Query, subscription=Subscription)
+
+
+@pytest.fixture
+def test_client(schema):
+    app = GraphQL(schema)
+
+    return TestClient(app)
+
+
+@pytest.fixture
+def test_client_keep_alive(schema):
+    app = GraphQL(schema, keep_alive=2)
+
+    return TestClient(app)
